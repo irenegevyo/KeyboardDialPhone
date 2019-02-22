a2cea411af91485570e73a86227ee35a69371d9f
irenegevyo <irenegevyo@gmail.com>
2019-02-22 14:07:28 +0700

    Initial commit| challenge 2

---
+++ app/src/main/java/com/example/keyboarddialphone/MainActivity.java
@@ -0,0 +1,51 @@
+package com.example.keyboarddialphone;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.KeyEvent;
+import android.view.inputmethod.EditorInfo;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = MainActivity.class.getSimpleName(); //untuk ngasih tau tempat ngambil tagnya
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        EditText editText = findViewById(R.id.text_main);
+        if (editText != null)
+            editText.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+                @Override
+                public boolean onEditorAction(TextView textView, int actionId, KeyEvent keyEvent) {
+                    boolean handled = false;
+                    if (actionId == EditorInfo.IME_ACTION_SEND) {
+                        dialNumber();
+                        handled = true;
+                    }
+                    return handled;
+                }
+            });
+    }
+
+    private void dialNumber() {
+        EditText editText = findViewById(R.id.text_main);
+        String phoneNum = null;
+        if (editText != null) phoneNum = "tel:" + editText.getText().toString();
+        Log.d(TAG, "diaNumber:" + phoneNum);
+        Intent intent = new Intent(Intent.ACTION_DIAL);
+        intent.setData(Uri.parse(phoneNum));
+
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            startActivity(intent);
+        } else {
+            Log.d("ImplicitIntent", "Can't Handle This");
+        }
+    }
+}
